7904c15bd4e57abfbefcdf8f0c21657e9f233d40
UbaiddRaza <ubaiddraza@gmail.com>
2024-08-03 17:52:25 +0500

    variables, prompt and alert

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 console.log("Hello JavaScript");
+
+
+
+
+var username = ("ubaid raza")
+var birthDate = 30_05_2006
+var thisIs = "This is"
+var space = ("_")
+
+console.log(username + space + thisIs + space + birthDate)
+
+
+
+alert("This is an alert");
+var name = prompt("Enter your name")
+console.log(name)
