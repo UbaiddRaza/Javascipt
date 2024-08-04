672f7b6ca935a4713039ed6020328f00d1d1aa07
UbaiddRaza <ubaiddraza@gmail.com>
2024-08-04 17:59:38 +0500

    assignment operator

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-console.log("Hello JavaScript");
+// console.log("Hello JavaScript");
 
 
 
 
-var username = ("ubaid raza")
-var birthDate = 30_05_2006
-var thisIs = "This is"
-var space = ("_")
+// var username = ("ubaid raza")
+// var birthDate = 30_05_2006
+// var thisIs = "This is"
+// var space = ("_")
 
-console.log(username + space + thisIs + space + birthDate)
+// console.log(username + space + thisIs + space + birthDate)
 
 
 
-alert("This is an alert");
-var name = prompt("Enter your name")
-console.log(name)
+// alert("This is an alert");
+// var name = prompt("Enter your name")
+// console.log(name)
+
+
+// var username = ("Abdullah")
+// var age = 21
+
+// console.log("my name is" , username , ("and my age is") , age )
+
+
+
+
+// num1 = 20
+// num2 = 30
+// var num3 = 0
+// num3 += num1 + num2
+// console.log(num3)
+
+
+
+// console.log(20 != 20) // data type not check not equal to
+// console.log(20 !== "20") // data type check not equal to
+// console.log(20 == "20") // data type not check equal to
+// console.log(20 === "20") // data type check equal to
+
+// console.log(10 < 5 && 10 < 5 )
+
